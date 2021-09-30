69810b9a6cbd57ece1c91eccdc5396ac55a91af7
Kota Kanazawa <kota0818@gmail.com>
2021-09-30 22:02:10 +0900

    add all_links query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,14 +4,10 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :all_links, [LinkType], null: false
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def all_links
+      Link.all
     end
   end
 end
