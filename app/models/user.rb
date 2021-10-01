54d096e1dd6c2fc2f8f7698e9f0afe643fcbcd9d
Kota Kanazawa <kota0818@gmail.com>
2021-10-01 09:38:58 +0900

    fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true, uniquness: true
+  validates :email, presence: true, uniqueness: true
 end
